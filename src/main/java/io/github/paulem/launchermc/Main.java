e504718518832bbcd72963de290d417d9889b5b7
Paulem <66211574+Paulem79@users.noreply.github.com>
2024-07-24 13:53:45 +0200

    Well, can't fix that

--- src/main/java/io/github/paulem/launchermc/Main.java
+++ src/main/java/io/github/paulem/launchermc/Main.java
@@ -3,22 +3,24 @@ package io.github.paulem.launchermc;
 import javafx.application.Application;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        new Thread(() -> {
-            try {
-                Class.forName("javafx.application.Application");
-            } catch (ClassNotFoundException e) {
-                // Mauvaise version de java
-                JOptionPane.showMessageDialog(
-                        null,
-                        "Votre version de Java ne semble pas être la bonne !",
-                        "Erreur",
-                        JOptionPane.ERROR_MESSAGE
-                );
-            }
-            Application.launch(Launcher.class, args);
-        }).start();
+        try {
+            Class.forName("javafx.application.Application");
+        } catch (ClassNotFoundException e) {
+            // Mauvaise version de java
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Votre version de Java ne semble pas être la bonne !",
+                    "Erreur",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+        Application.launch(Launcher.class, args);
     }
 }
